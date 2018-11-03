ab8b06955af23ea5626ef8800287dd2eb95b51f3
User <zipper@zipper.moe>
2018-11-03 08:14:15 -0400

    streaming data for efficiency

--- utils/blockchainutils.py
+++ utils/blockchainutils.py
@@ -12,3 +12,16 @@ def fileToBlockchain(f, dhs = None): # f is the filename, dhs is the diffie hell
         if len(cur) == 0:
             break
     return fBlockchain
+
+def fileToBlockchainStream(f, dhs = None): # f is the filename, dhs is the diffie hellman secret (default none)
+    fBytes = open(f, 'rb')
+    fBlockchain = Blockchain(dhs)
+    cur = fBytes.read(64)
+    while True:
+        fBlockchain.add(cur)
+        if fBlockchain.size > 1:
+            yield fBlockchain.popHead()
+        cur = fBytes.read(64)
+        if len(cur) == 0:
+            yield fBlockchain.popHead()
+            break
