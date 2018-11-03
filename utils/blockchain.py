91e12e80fe5ce5d1258070e223e4903ff5f99e34
User <zipper@zipper.moe>
2018-11-03 07:49:55 -0400

    utils and diffie hellman support enhanced

--- utils/blockchain.py
+++ utils/blockchain.py
@@ -3,7 +3,7 @@ from Crypto.Cipher import AES
 from Crypto import Random
 
 START_HASH = bcrypt.hashpw("2SECURE4UPROTOCOL", bcrypt.gensalt())
-DATA_PAD = '0' * 15
+DATA_PAD = 64
 
 class Block:
     def __init__(self, data):
@@ -12,12 +12,19 @@ class Block:
         self.data = data
 
 class TFSUBlock(Block):
-    def __init__(self, prevhash, data, dhscrypt = None):
-        data = DATA_PAD + data
+    def __init__(self, prevhash, data, dhscrypt = None, dhsdec = None):
+        data = data + (chr(1) * (DATA_PAD - len(data)))
+        self.dhsdec = dhsdec
         curhash = self.newHash(prevhash, data)
         encdat = self.encData(data, dhscrypt)
         dat = (curhash, encdat) # Hash, encrypted data
         Block.__init__(self, dat)
+
+    def __str__(self):
+        if (self.dhsdec == None):
+            return str(self.data[1])
+        else:
+            return str(self.dhsdec.decrypt(self.data[1]))
 
     def newHash(self, prevhash, data):
         return bcrypt.hashpw(prevhash + data, bcrypt.gensalt())
@@ -28,11 +35,11 @@ class TFSUBlock(Block):
         else:
             return dhscrypt.encrypt(data)
 
-
 class Blockchain:
     def __init__(self, dhs = None):
         self.head = None
         self.tail = None
+        self.size = 0
         if dhs == None:
             self.dhscrypt = None
             self.dhsdec = None
@@ -46,16 +53,26 @@ class Blockchain:
             self.dhscrypt = AES.new(key, AES.MODE_CBC, iv)
             self.dhsdec = AES.new(key, AES.MODE_CBC, iv)
 
+    def __str__(self):
+        ret = [None] * self.size
+        cur = self.head
+        for i in xrange(self.size):
+            ret[i] = str(cur)
+            cur = cur.next
+        return '->'.join(ret)
+
+
     def add(self, data):
         if self.head == None:
-            new_node = TFSUBlock(START_HASH, data, self.dhscrypt)
+            new_node = TFSUBlock(START_HASH, data, self.dhscrypt, self.dhsdec)
             self.head = self.tail = new_node
         else:
-            new_node = TFSUBlock(self.tail.data[0], data, self.dhscrypt)
+            new_node = TFSUBlock(self.tail.data[0], data, self.dhscrypt, self.dhsdec)
             new_node.prev = self.tail
             new_node.next = None
             self.tail.next = new_node
             self.tail = new_node
+        self.size += 1
 
     def popHead(self):
         ret = self.head
@@ -64,9 +81,11 @@ class Blockchain:
         elif self.tail == ret:
             self.head = None
             self.tail = None
+            self.size -= 1
             return ret
         self.head = ret.next
         self.head.prev = None
+        self.size -= 1
         return ret
 
     def popTail(self):
@@ -76,7 +95,9 @@ class Blockchain:
         elif self.head == ret:
             self.head = None
             self.tail = None
+            self.size -= 1
             return ret
         self.tail = ret.prev
         self.tail.next = None
+        self.size -= 1
         return ret
---
+++ utils/blockchainutils.py
@@ -0,0 +1,14 @@
+from blockchain import Blockchain
+
+BLOCK_SIZE = 64
+
+def fileToBlockchain(f, dhs = None): # f is the filename, dhs is the diffie hellman secret (default none)
+    fBytes = open(f, 'rb')
+    fBlockchain = Blockchain(dhs)
+    cur = fBytes.read(64)
+    while True:
+        fBlockchain.add(cur)
+        cur = fBytes.read(64)
+        if len(cur) == 0:
+            break
+    return fBlockchain
